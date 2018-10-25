d4d0010a1832f4e6f467d4f2f8bb859462683ecf
tomoyukikashiro <ksrtmyk@gmail.com>
2018-10-25 22:23:01 +0900

    feat(amp): Added dir

---
+++ amp/gatsby-node.js
@@ -0,0 +1,43 @@
+const path = require("path")
+const _ = require("lodash")
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const BlogPost = path.resolve("src/templates/blog.js")
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 2000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              slug
+              tags
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    const posts = result.data.allMarkdownRemark.edges
+
+    // Create post detail pages
+    posts.forEach(({ node }) => {
+      createPage({
+        path: `/post/${node.frontmatter.slug}`,
+        component: BlogPost,
+        context: {
+          slug: node.frontmatter.slug,
+        },
+      })
+    })
+  })
+}
