084a1f9bbdc0119fa95b86ea2e5c5b83326530ef
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2018-11-05 20:50:45 +0900

    feat(amp): Added search meta (#31)

--- amp/src/html.js
+++ amp/src/html.js
@@ -3,12 +3,22 @@ import PropTypes from "prop-types"
 
 export default class HTML extends React.Component {
   render() {
+    const analyticsConfig = { "vars": { "account": "UA-50867411-3"}, "triggers": {"trackPageview": {"on": "visible", "request": "pageview"}}}
     return (
       <html {...this.props.htmlAttributes}>
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="google-site-verification" content="F0csCPag4kfg-qefukr_0b2yJxXNbTtaJ11Cv7pVUcU" />
           {this.props.headComponents}
+          <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
+          <amp-analytics type="googleanalytics">
+            <script
+              type="application/json"
+              dangerouslySetInnerHTML={{
+                __html: JSON.stringify(analyticsConfig)
+              }} />
+          </amp-analytics>
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
