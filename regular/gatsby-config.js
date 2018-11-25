acad7f5c5c6a3ba700ea2f31995379db90373518
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2018-11-25 19:59:32 +0900

    feat(chore): Added web manifest (#53)

--- regular/gatsby-config.js
+++ regular/gatsby-config.js
@@ -67,6 +67,29 @@ module.exports = {
       }
     },
     {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Tomoyuki Kashiro\'s Blog',
+        start_url: 'TK Blog',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        display: 'standalone',
+        legacy: true,
+        icons: [
+          {
+            "src": "/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/android-chrome-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          }
+        ],
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
