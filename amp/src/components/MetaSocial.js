69a7fa72037e47295f1ae7006ad4858cbe529d37
tomoyukikashiro <ksrtmyk@gmail.com>
2018-12-21 14:05:30 +0900

    feat(amp): multiple language

--- amp/src/components/MetaSocial.js
+++ amp/src/components/MetaSocial.js
@@ -1,57 +1,39 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 
-const MetaSocial = ({title, description, type, url, image, tags=[], published}) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            siteUrl
-            ampUrl
-            author
-            twitterUserName
-          }
-        }
-      }
-    `}
-    render={ ({site: { siteMetadata }} ) => (
-      <Helmet>
-        {/* BASIC */}
-        <meta property="og:title" content={ title.trim() } />
-        <meta property="og:type" content={ type.trim() } />
-        <meta property="og:image" content={ `${ siteMetadata.ampUrl }${ image.trim() }` } />
-        <meta property="og:url" content={ url.trim() } />
+const MetaSocial = ({title, description, type, url, image, tags=[], published, lang = 'en-US', site}) => (
+  <Helmet>
+    {/* BASIC */}
+    <meta property="og:title" content={ title } />
+    <meta property="og:type" content={ type } />
+    <meta property="og:image" content={ `${ site.ampUrl }${ image.trim() }` } />
+    <meta property="og:url" content={ url } />
 
-        {/* OPTIONAL */}
-        <meta property="og:description" content={ description.trim().replace(/"/g, '') } />
-        <meta property="og:locale" content="en" />
-        <meta property="og:site_name" content={ siteMetadata.title } />
+    {/* OPTIONAL */}
+    <meta property="og:description" content={ description.replace(/"/g, '') } />
+    <meta property="og:locale" content={ lang } />
+    <meta property="og:site_name" content={ site.title } />
 
-        {/* og:type:article */}
-        { published
-          ? < meta property="article:published_time" content={ published.toISOString() } />
-          : null
-        }
-        { type === 'article'
-          ? <meta property="profile:username" content={ siteMetadata.author } />
-          : null
-        }
-        { tags.map(tag => (
-          <meta key={ tag.trim() } property="article:tag" content={ tag.trim() } />
-        )) }
+    {/* og:type:article */}
+    { published
+      ? < meta property="article:published_time" content={ published } />
+      : null
+    }
+    { type === 'article'
+      ? <meta property="profile:username" content={ site.author } />
+      : null
+    }
+    { tags.map(tag => (
+      <meta key={ tag } property="article:tag" content={ tag } />
+    )) }
 
-        {/* twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content={ `@${ siteMetadata.twitterUserName }` } />
-        <meta name="twitter:title" content={ title.trim() } />
-        <meta name="twitter:description" content={ description.trim().replace(/"/g, '') } />
-        <meta name="twitter:image" content={ `${ siteMetadata.ampUrl }${ image.trim() }` } />
-      </Helmet>
-    )}
-  />
+    {/* twitter */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:creator" content={ `@${ site.twitterUserName }` } />
+    <meta name="twitter:title" content={ title } />
+    <meta name="twitter:description" content={ description.replace(/"/g, '') } />
+    <meta name="twitter:image" content={ `${ site.ampUrl }${ image.trim() }` } />
+  </Helmet>
 )
 
 export default MetaSocial
--- amp/src/components/ld_json/ArticleBreadCrumb.js
+++ amp/src/components/ld_json/ArticleBreadCrumb.js
@@ -1,52 +1,38 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
 import { headerBgUrl } from '../../utils/image'
 import Helmet from 'react-helmet'
 
-const ArticleBreadCrumb = ({ post }) => (
-  <StaticQuery
-    query={ graphql`
-      query {
-        site {
-          siteMetadata {
-            siteUrl
-          }
-        }
-      }
-    `}
-    render={ ({site: { siteMetadata }} ) => (
-      <Helmet>
-        <script type="application/ld+json">
+const ArticleBreadCrumb = ({ post, site }) => (
+  <Helmet>
+    <script type="application/ld+json">
+      {
+        `{
+        "@context": "http://schema.org",
+        "@type": "BreadcrumbList",
+        "itemListElement": [
           {
-            `{
-            "@context": "http://schema.org",
-            "@type": "BreadcrumbList",
-            "itemListElement": [
-              {
-                "@type": "ListItem",
-                "position": 1,
-                "item": {
-                  "@id": "${ siteMetadata.siteUrl }/",
-                  "name": "HOME",
-                  "image": "${ siteMetadata.siteUrl }${ headerBgUrl() }"
-                }
-              },
-              {
-                "@type": "ListItem",
-                "position": 2,
-                "item": {
-                  "@id": "${ siteMetadata.siteUrl }/post/${ post.slug }/",
-                  "name": "${ post.title }",
-                  "image": "${ siteMetadata.siteUrl }${ headerBgUrl(new Date(post.date).getDate()) }"
-                }
-              }
-            ]
-          }`
+            "@type": "ListItem",
+            "position": 1,
+            "item": {
+              "@id": "${ site.ampUrl }/",
+              "name": "HOME",
+              "image": "${ site.ampUrl }${ headerBgUrl() }"
+            }
+          },
+          {
+            "@type": "ListItem",
+            "position": 2,
+            "item": {
+              "@id": "${ site.ampUrl }${post.path()}/",
+              "name": "${ post.title }",
+              "image": "${ site.ampUrl }${ headerBgUrl(post.date.getDate()) }"
+            }
           }
-        </script>
-      </Helmet>
-    )}
-  />
+        ]
+      }`
+      }
+    </script>
+  </Helmet>
 )
 
 export default ArticleBreadCrumb
