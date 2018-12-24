4969379d67d350c1b657e0763b2fdffd1373ab11
tomoyukikashiro <ksrtmyk@gmail.com>
2018-12-24 19:53:22 +0900

    fix(cms): settings

--- regular/src/cms/preview-templates/BlogPostPreview.js
+++ regular/src/cms/preview-templates/BlogPostPreview.js
@@ -4,7 +4,7 @@ import { BlogPostTemplate } from '../../templates/blog'
 const BlogPostPreview = ({ entry, widgetFor }) => (
   <BlogPostTemplate
     author="Tomoyuki Kashiro"
-    date={entry.getIn(['data', 'date'])}
+    date={`${entry.getIn(['data', 'date'])}`}
     title={entry.getIn(['data', 'title'])}
     slug={entry.getIn(['data', 'slug'])}
     lang={entry.getIn(['data', 'lang'])}
