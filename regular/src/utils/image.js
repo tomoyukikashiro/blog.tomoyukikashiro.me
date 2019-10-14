59ef6893b87b5bdcb17c2b3b2c0d7428dcb17277
tomoyukikashiro <ksrtmyk@gmail.com>
2019-10-14 14:43:56 +0900

    fix(amp): remove amp dir

--- regular/src/utils/image.js
+++
@@ -1,7 +0,0 @@
-export const headerBgUrl = (day) => {
-  return day ? `/images/${ day % 7 }.jpg` : `/images/home.jpg`
-}
-
-export const headerBgClass = (day) => {
-  return `header__bg_${ day % 7 }`
-}
---
+++ src/utils/dist/post.js
@@ -0,0 +1,86 @@
+"use strict";
+
+exports.__esModule = true;
+exports.default = void 0;
+
+// import moment from 'moment'
+class Post {
+  constructor(node) {
+    this.node = node;
+  }
+
+  get key() {
+    const key = `${this.lang}-${this.slug}`;
+    return key || Date.now();
+  }
+
+  get title() {
+    return this.node.frontmatter.title ? this.node.frontmatter.title.trim() : '';
+  }
+
+  get date() {
+    if (!this.node.frontmatter.date) return;
+    return new Date(this.node.frontmatter.date.trim());
+  }
+
+  get type() {
+    return 'article';
+  }
+
+  get isoDate() {
+    return this.date.toISOString();
+  }
+
+  get formatDate() {
+    return this.date.toLocaleString('en', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    }); // return moment(this.date.toISOString()).format('dddd LL')
+  }
+
+  get summary() {
+    return this.node.frontmatter.summary ? this.node.frontmatter.summary.trim() : '';
+  }
+
+  get slug() {
+    return this.node.frontmatter.slug ? this.node.frontmatter.slug.trim() : '';
+  }
+
+  get lang() {
+    return this.node.frontmatter.lang ? this.node.frontmatter.lang.trim() : '';
+  }
+
+  get isEn() {
+    return this.lang === Post.LANGS.EN;
+  }
+
+  get isJa() {
+    return this.lang === Post.LANGS.JA;
+  }
+
+  get alternativeLang() {
+    return this.isEn ? Post.LANGS.JA : Post.LANGS.EN;
+  }
+
+  path(lang) {
+    const langPath = (lang || this.lang) === Post.LANGS.JA ? '/ja/' : '/';
+    return `/post${langPath}${this.slug}`;
+  }
+
+  get alternativeLangPath() {
+    return this.path(this.alternativeLang);
+  }
+
+  get html() {
+    return this.node.html;
+  }
+
+}
+
+exports.default = Post;
+Post.LANGS = {
+  JA: 'ja',
+  EN: 'en'
+};
