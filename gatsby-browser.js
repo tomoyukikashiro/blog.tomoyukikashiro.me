f4534aab32a0cade87b9cc27bd9736a09dd95073
tomoyukikashiro <ksrtmyk@gmail.com>
2019-10-19 12:00:51 +0900

    fix(style): markdown

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,7 @@
 require('reset-css/reset.css')
 require('github-markdown-css/github-markdown.css')
 require('prismjs/themes/prism-tomorrow.css')
+require('./src/assets/styles/prism.css')
 require('./src/assets/styles/variable.css')
 require('./src/assets/styles/base.css')
 require('./src/assets/styles/util.css')
